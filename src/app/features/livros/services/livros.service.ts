9f3766f9386ae610254c3ecebfac02fb15105e3e
AlanBeckG1 <alanbeckg1@gmail.com>
2026-08-28 16:38:13 -0300

    Atualizacoes de APIurl2

--- src/app/features/livros/services/livros.service.ts
+++ src/app/features/livros/services/livros.service.ts
@@ -8,7 +8,7 @@ import { Livro, StatusLivro } from "../models/livro";
 })
 export class LivrosService {
   private readonly http = inject(HttpClient);
-  private readonly apiUrl = "https://api-de-livros-3i55.onrender.com";
+  private readonly apiUrl = "https://api-de-livros-3i55.onrender.com/api/livros";
 
   listar(): Promise<Livro[]> {
     return firstValueFrom(this.http.get<Livro[]>(this.apiUrl));
